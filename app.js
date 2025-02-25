426347c01c92b6c13afe266ca6c8cde73c0d01cf
siddharth <siddharthmthapa@gmail.com>
2025-02-25 14:25:15 +0530

    initial push

---
+++ app.js
@@ -0,0 +1,25 @@
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector("nav");
+
+    if (window.scrollY === 0 ) {
+        navbar.classList.remove("scrolled");
+    } else {
+        navbar.classList.add("scrolled");
+    }
+});
+
+
+//accordion
+const items = document.querySelectorAll('.accordion button');
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
